a83efa44a5fc058d5e51ae401e325a44bdaa9813
KgMLe <kgodisholebopa@gmail.com>
2023-08-14 14:42:05 +0200

    Routes

--- API/model/index.js
+++ API/model/index.js
@@ -1,1 +1,9 @@
 // import all models
+const Users = require('./users')
+const Orders = require ('./orders')
+const Books = require ('./books')
+const BookAuthors = require ('./bookAuthor')
+// Export All Objects
+module.exports = {
+    users: new Users // creating a new object or the object of a class
+}
