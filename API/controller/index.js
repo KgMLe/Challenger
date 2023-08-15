9d8c21f60ccd5cb846d3b5913b78db3e41288f64
KgMLe <kgodisholebopa@gmail.com>
2023-08-15 13:41:24 +0200

    orders

--- API/controller/index.js
+++ API/controller/index.js
@@ -3,6 +3,7 @@ const bodyParser = require ('body-parser')
 const routes = express.Router ()
 // import all model's objects
 const {users} = require ('../model')
+const {orders} = require ('../model')
 
 routes.get ('/users', (req,res)=>{
    users.fetchUsers(req, res)
@@ -31,11 +32,43 @@ routes.delete ('/user/:id', (req, res)=>{
     users.deleteUser(req, res)
 })
 
-module.exports = {
-    express, routes
-}
 
 routes.post('/login',
 bodyParser.json(), (req, res)=>{
     users.login(req, res)
 })
+
+// ORDERS ROUTES 
+
+routes.get ('/orders', (req,res)=>{
+    orders.fetchOrders(req, res)
+ })
+ 
+ routes.get ('/orders/:id', (req, res)=>{
+     orders.selectOrder (req, res)
+ })
+ 
+ routes.get ('/orders/orderbydate',  (req, res)=>{
+     orders.orderDate(req, res)
+ })
+ 
+ routes.put ('/order/:id', bodyParser.json(),
+ (req, res)=>{
+     orders.updateOrder(req, res)
+ })
+ 
+ routes.patch ('/order/:id', bodyParser.json(),
+ (req, res)=>{
+     orders.updateOrder(req, res)
+ })
+
+ routes.delete ('/order/:id', (req, res)=>{
+    orders.deleteOrder(req, res)
+})
+
+//books
+
+
+module.exports = {
+    express, routes
+}
--- API/model/orders.js
+++ API/model/orders.js
@@ -1,6 +1,85 @@
 // import Orders
 class Orders{
+fetchOrders (req,res){
+   const query = 
+   `
+   SELECT orderID, userID, bookID, orderDate
+   FROM Orders;
+   ` 
+   db.query(query,
+    (err, results)=>{
+       if(err) throw err
+       res.json ({
+           status: res.statusCode, // collects the status code from the server
+           results
+       })
+    } )
+}
 
+selectOrder (req,res){
+        const query =`
+        SELECT o.orderID, o.userID, b.bookID, b.bookTitle, o.orderDate
+        FROM Orders o
+        INNER JOIN Books b
+        using (bookID)
+        WHERE orderID = ${req.params.id};
+        `
+        db.query(query,
+            (err, result)=>{
+                if(err) throw err
+                res.json ({
+                    status: res.statusCode,
+                    result
+                })
+     })
+    }
+
+orderDate(req,res){
+    const query = `
+    SELECT orderID, userID, bookID, orderDate
+    FROM Orders
+    ORDER BY orderDate;
+    `
+    db.query(query,
+        (err, result)=>{
+            if(err) throw err
+            res.json ({
+                status: res.statusCode,
+                result
+            })
+  })
+}
+
+updateOrder (req,res){
+    const query =`
+    UPDATE Orders
+    SET ?
+    WHERE orderID = ?;
+    `
+    db.query(query, 
+        [req.body, req.params.id],
+        (err)=>{
+            if(err) throw err
+            res.json ({
+                status: res.statusCode,
+                msg: "Order updated"
+            })
+        })
+}
+
+deleteOrder (req, res){
+    const query =`
+    UPDATE FROM Orders
+    WHERE ordersID = ${req.params.id};
+    `
+    db.query(query, (err)=>{
+            if(err) throw err
+            res.json ({
+                status: res.statusCode,
+                msg: "User record deleted"
+            })
+        })
+}
 }
 
 module.exports = Orders
